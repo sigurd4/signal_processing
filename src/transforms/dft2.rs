de180fceade1be97df9c75e72618d487b4350364
Sigurd Sætherø Spangelo <sigurd-s@online.no>
2024-04-06 18:43:38 +0200

    Rename dft matrix to dft2

--- src/transforms/dft2.rs
+++ src/transforms/dft2.rs
@@ -5,22 +5,22 @@ use num::{complex::ComplexFloat, Complex};
 
 use crate::{Matrix, OwnedMatrix, OwnedLists};
 
-pub trait DFTMatrix<T>: Matrix<T>
+pub trait DFT2<T>: Matrix<T>
 where
     T: ComplexFloat,
     Self::Mapped<Complex<T::Real>>: Matrix<Complex<T::Real>>
 {
-    fn dftmtx(&self) -> <<Self::Mapped<Complex<T::Real>> as Matrix<Complex<T::Real>>>::Transpose as Matrix<Complex<T::Real>>>::Transpose;
+    fn dft2(&self) -> <<Self::Mapped<Complex<T::Real>> as Matrix<Complex<T::Real>>>::Transpose as Matrix<Complex<T::Real>>>::Transpose;
 }
 
-impl<T, M> DFTMatrix<T> for M
+impl<T, M> DFT2<T> for M
 where
     T: ComplexFloat + Into<Complex<T::Real>>,
     M: Matrix<T>,
     M::Mapped<Complex<T::Real>>: OwnedMatrix<Complex<T::Real>> + 'static,
     Complex<T::Real>: ComplexFloat<Real = T::Real> + MulAssign + AddAssign
 {
-    fn dftmtx(&self) -> <<Self::Mapped<Complex<T::Real>> as Matrix<Complex<T::Real>>>::Transpose as Matrix<Complex<T::Real>>>::Transpose
+    fn dft2(&self) -> <<Self::Mapped<Complex<T::Real>> as Matrix<Complex<T::Real>>>::Transpose as Matrix<Complex<T::Real>>>::Transpose
     {
         let mut h = self.map_to_owned(|&h| h.into());
         for h in h.as_mut_slice2()
