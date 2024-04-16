703153e3befbc3f7e1da1ce9ec519bf75794be87
Sigurd Sætherø Spangelo <sigurd-s@online.no>
2024-04-16 17:19:55 +0200

    Generalize peak-to-peak

--- src/analysis/peak_to_peak.rs
+++ src/analysis/peak_to_peak.rs
@@ -1,29 +1,31 @@
-use num::Float;
+use core::ops::Sub;
+
+use num::Zero;
 
 use crate::{List, ListOrSingle, Lists};
 
 pub trait PeakToPeak<'a, T>: Lists<T>
 where
-    T: Float
+    T: PartialOrd + Sub
 {
-    fn peak_to_peak(&'a self) -> Self::RowsMapped<T>;
+    fn peak_to_peak(&'a self) -> Self::RowsMapped<<T as Sub>::Output>;
 }
 
 impl<'a, T, L> PeakToPeak<'a, T> for L
 where
     L: Lists<T, RowView<'a>: List<T>> + 'a,
-    T: Float + 'a
+    T: PartialOrd + Sub + Zero + Clone + 'a
 {
-    fn peak_to_peak(&'a self) -> Self::RowsMapped<T>
+    fn peak_to_peak(&'a self) -> Self::RowsMapped<<T as Sub>::Output>
     {
         self.map_rows_to_owned(|x| {
             let x = x.to_vec();
             x.iter()
-                .map(|&x| x)
-                .reduce(T::max)
+                .map(|x| x.clone())
+                .reduce(|a, b| if a == a && a >= b {a} else {b})
                 .unwrap_or(T::zero())
             - x.into_iter()
-                .reduce(T::min)
+                .reduce(|a, b| if a == a && a <= b {a} else {b})
                 .unwrap_or(T::zero())
         })
     }
