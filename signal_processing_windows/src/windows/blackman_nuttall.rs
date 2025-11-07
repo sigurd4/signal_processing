c47992a704980d7a2927ee39ef42cbb6d9644796
sigurd4 <sigurd-s@online.no>
2025-11-08 00:52:07 +0100

    Move windowing stuff in a separate crate

---
+++ signal_processing_windows/src/windows/blackman_nuttall.rs
@@ -0,0 +1,66 @@
+use array_trait::length::{self, LengthValue};
+use bulks::Bulk;
+use num::{Float, complex::ComplexFloat, traits::FloatConst};
+
+use crate::generators::window::{WindowGen, WindowRange};
+
+pub struct BlackmanNuttall;
+
+impl<T, N> WindowGen<T, N> for BlackmanNuttall
+where
+    T: ComplexFloat,
+    N: LengthValue
+{
+    type Output = impl Bulk<Item = T>;
+
+    fn window_gen(&self, numtaps: N, range: WindowRange) -> Self::Output
+    {
+        let mut i = 0;
+        let n = length::value::len(numtaps);
+        let m = match range
+        {
+            WindowRange::Symmetric => n - 1,
+            WindowRange::Periodic => n,
+        };
+        let a0 = T::from(0.3635819).unwrap();
+        let a1 = T::from(0.4891775).unwrap();
+        let a2 = T::from(0.1365995).unwrap();
+        let a3 = T::from(0.0106411).unwrap();
+        bulks::repeat_n_with(|| {
+            let z1 = (T::TAU()*T::from(i).unwrap()/T::from(m).unwrap()).cos();
+            let z2 = (T::TAU()*T::from(i*2).unwrap()/T::from(m).unwrap()).cos();
+            let z3 = (T::TAU()*T::from(i*3).unwrap()/T::from(m).unwrap()).cos();
+            i += 1;
+            a0 - a1*z1 + a2*z2 - a3*z3
+        }, numtaps)
+    }
+}
+
+#[cfg(test)]
+mod test
+{
+    use core::f64::consts::{PI, TAU};
+
+    use linspace::Linspace;
+
+    use crate::{plot, generators::window::{WindowGen, WindowRange}, analysis::FreqZ, systems::Tf};
+
+    use super::BlackmanNuttall;
+
+    #[test]
+    fn test()
+    {
+        const N: usize = 1024;
+        let w: [_; N/2] = BlackmanNuttall.window_gen((), WindowRange::Symmetric);
+        let n = (0.0..1.0).linspace_array();
+
+        plot::plot_curves("g(n/N)", "plots/windows/g_n_blackman_nuttall.png", [&n.zip(w)]).unwrap();
+
+        let (mut w_f, mut omega): ([_; N], _) = Tf::new(w, ()).freqz((), false);
+        omega.map_assign(|omega| (omega + PI) % TAU - PI);
+        w_f.rotate_right(N/2);
+        omega.rotate_right(N/2);
+        
+        plot::plot_curves("G(e^jw)", "plots/windows/g_f_blackman_nuttall.png", [&omega.zip(w_f.map(|w| 20.0*w.norm().log10()))]).unwrap();
+    }
+}
--- signal_processing_windows/src/windows/mod.rs
+++ signal_processing_windows/src/windows/mod.rs
@@ -26,3 +26,23 @@ moddef::moddef!(
         welch
     }
 );
+
+use array_trait::length::LengthValue;
+use bulks::Bulk;
+use num::complex::ComplexFloat;
+
+pub enum WindowRange
+{
+    Symmetric,
+    Periodic
+}
+
+pub const trait WindowGen<T, N>
+where
+    T: ComplexFloat,
+    N: LengthValue
+{
+    type Output: Bulk<Item = T>;
+
+    fn window_gen(&self, numtaps: N, range: WindowRange) -> Self::Output;
+}
