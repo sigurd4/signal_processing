9bcad505bf9ea0d216791a426a5cc0a932594e09
sigurd4 <sigurd-s@online.no>
2025-12-07 02:45:18 +0100

    Readd bit permutations

--- signal_fourier/src/lib.rs
+++ signal_fourier/src/lib.rs
@@ -2,9 +2,14 @@
 #![feature(fn_traits)]
 #![feature(const_trait_impl)]
 #![feature(const_convert)]
+#![feature(try_trait_v2)]
+#![feature(const_try)]
 #![feature(trusted_random_access)]
+#![feature(const_destruct)]
 #![feature(core_intrinsics)]
 #![feature(specialization)]
+
+use bulks::{Bulk, InplaceBulk};
 
 #[cfg(not(any(feature = "std", feature = "libm")))]
 compile_error!("Either the \"std\" or the \"libm\" feature must be enabled to compile");
@@ -15,11 +20,32 @@ moddef::moddef!(
         permute
     },
     flat(pub) mod {
-        fft_output,
-        inplace_bulk,
-        scratch_space
+        //scratch_space
     }
 );
+
+pub const trait FourierBulk: ~const Bulk
+{
+    fn digit_rev_permute(self, radix: usize) -> Self
+    where
+        Self: for<'a> ~const InplaceBulk<'a, ItemMut = &'a mut <Self as IntoIterator>::Item> + Sized
+    {
+        permute::digit_rev_permute(self, radix)
+    }
+
+    fn bit_rev_permute(self) -> Self
+    where
+        Self: for<'a> ~const InplaceBulk<'a, ItemMut = &'a mut <Self as IntoIterator>::Item> + Sized
+    {
+        self.digit_rev_permute(2)
+    }
+}
+impl<I> const FourierBulk for I
+where
+    I: ~const Bulk
+{
+
+}
 
 #[inline]
 pub const fn is_power_of(n: usize, r: usize) -> bool
--- signal_fourier/src/permute.rs
+++ signal_fourier/src/permute.rs
@@ -1,65 +1,52 @@
-use core::iter::TrustedRandomAccess;
+use bulks::InplaceBulk;
 
-use bulks::Bulk;
-
-pub trait DigitRevPermutation: MutRandomAccessBulk
+pub(crate) const fn digit_rev_permute<I, T>(mut bulk: I, radix: usize) -> I
+where
+    I: for<'a> ~const InplaceBulk<'a, ItemMut = &'a mut T>
 {
-    fn digit_rev_permutation(&mut self, radix: usize) -> &mut Self
-    {
-        let len = self.len();
-        if len <= radix
-        {
-            return self;
-        }
-        assert!(crate::is_power_of(len, radix), "Length must be a power of radix.");
-
-        let mut i = 1;
-        let mut j = len/radix + 1;
-        while i < len - 1
-        {
-            if i < j - 1
-            {
-                core::mem::swap(self.get_mut(i), self.get_mut(j - 1));
-            }
-            let mut k = len/radix;
-            while k*(radix - 1) < j
-            {
-                j -= k*(radix - 1);
-                k /= radix;
-            }
-            j += k;
-            i += 1;
-        }
-        self
-    }
-}
-
-fn digit_rev_permutation<T>(slice: &mut [T], radix: usize)
-{
-    let len = slice.len();
+    let len = bulk.len();
     if len <= radix
     {
-        return;
+        return bulk;
     }
-    assert!(crate::is_power_of(len, radix), "Length must be a power of radix.");
 
     let mut i = 1;
-    let mut j = len/radix + 1;
+    let mut j = len/radix;
     while i < len - 1
     {
-        if i < j - 1
+        if i < j
         {
-            unsafe {
-                core::ptr::swap_nonoverlapping(slice.as_mut_ptr().add(i), slice.as_mut_ptr().add(j - 1), 1);
-            }
+            bulk.swap_inplace(i, j);
         }
         let mut k = len/radix;
-        while k*(radix - 1) < j
+        while k*(radix - 1) <= j
         {
             j -= k*(radix - 1);
             k /= radix;
         }
         j += k;
         i += 1;
+    }
+    bulk
+}
+
+#[cfg(test)]
+mod test
+{
+    use bulks::{Bulk, IntoBulk};
+
+    use crate::FourierBulk;
+
+    #[test]
+    fn it_works()
+    {
+        let a = [0, 1, 2, 3, 4, 5, 6, 7];
+
+        for x in a.into_bulk()
+            .bit_rev_permute()
+            .collect_array()
+        {
+            print!("{x:03b} ")
+        }
     }
 }
