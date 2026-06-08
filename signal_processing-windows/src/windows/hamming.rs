1a0de1642eccabb40783e19b5c419622e2873911
sigurd4 <sigurd-s@online.no>
2026-06-08 12:37:56 +0200

    Add hamming and hann windows

--- signal_processing-windows/src/windows/hamming.rs
+++ signal_processing-windows/src/windows/hamming.rs
@@ -1,89 +1,44 @@
-use array_math::ArrayOps;
-use num::{traits::FloatConst, Float};
+use array_trait::length::Length;
+use num_traits::{Float, FloatConst, Zero};
 
-use crate::generators::window::{WindowGen, WindowRange};
+use crate::{Shape, WindowFn};
 
+#[derive(Clone, Copy)]
 pub struct Hamming;
 
-impl<T, const N: usize> WindowGen<T, [T; N], ()> for Hamming
+impl<L, T> WindowFn<L> for Hamming
 where
+    L: Length<Elem = T> + ?Sized,
     T: Float + FloatConst
 {
-    type Output = [T; N];
+    type Functor = impl Fn(usize) -> T;
 
-    fn window_gen(&self, (): (), r: WindowRange) -> Self::Output
+    fn window_fn(self, len: L::Value, range: Shape) -> Self::Functor
     {
-        if N <= 1
-        {
-            return [T::one(); N]
-        }
-
-        let m = match r
-        {
-            WindowRange::Symmetric => N - 1,
-            WindowRange::Periodic => N,
-        };
+        let m = range.window_len(len);
 
         let a0 = T::from(25.0/46.0).unwrap();
-        core::array::from_fn(|i| {
+
+        move |i| {
+            if m.is_zero()
+            {
+                return T::one()
+            }
+
             let z = (T::TAU()*T::from(i).unwrap()/T::from(m).unwrap()).cos();
             a0 - (T::one() - a0)*z
-        })
-    }
-}
-impl<T> WindowGen<T, Vec<T>, usize> for Hamming
-where
-    T: Float + FloatConst
-{
-    type Output = Vec<T>;
-
-    fn window_gen(&self, n: usize, r: WindowRange) -> Self::Output
-    {
-        if n <= 1
-        {
-            return vec![T::one(); n]
         }
-
-        let m = match r
-        {
-            WindowRange::Symmetric => n - 1,
-            WindowRange::Periodic => n,
-        };
-
-        let a0 = T::from(25.0/46.0).unwrap();
-        (0..n).map(|i| {
-            let z = (T::TAU()*T::from(i).unwrap()/T::from(m).unwrap()).cos();
-            a0 - (T::one() - a0)*z
-        }).collect()
     }
 }
 
 #[cfg(test)]
 mod test
 {
-    use core::f64::consts::{PI, TAU};
-
-    use array_math::ArrayOps;
-    use linspace::Linspace;
-
-    use crate::{plot, gen::window::{WindowGen, WindowRange}, analysis::FreqZ, systems::Tf};
-
-    use super::Hamming;
+    use crate::{tests, windows::Hamming};
 
     #[test]
     fn test()
     {
-        const N: usize = 1024;
-        let w: [_; N/2] = Hamming.window_gen((), WindowRange::Symmetric);
-        let n = (0.0..1.0).linspace_array();
-
-        plot::plot_curves("g(n/N)", "plots/windows/g_n_hamming.png", [&n.zip(w)]).unwrap();
-
-        let (mut w_f, mut omega): ([_; N], _) = Tf::new(w, ()).freqz((), false);
-        omega.map_assign(|omega| (omega + PI) % TAU - PI);
-        w_f.rotate_right(N/2);
-        omega.rotate_right(N/2);
-        
-        plot::plot_curves("G(e^jw)", "plots/windows/g_f_hamming.png", [&omega.zip(w_f.map(|w| 20.0*w.norm().log10()))]).unwrap();
+        tests::plot_window(Hamming)
     }
 }
